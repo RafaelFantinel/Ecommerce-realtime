4d753cc5917f0cb813f3abf2814b571daa578d8e
RafaelFantinel <rafaeldeoliveirafantinel@hotmail.com>
2020-04-16 23:23:16 -0300

    relacionamento user_image

--- database/migrations/1587089984920_user_image_fk_schema.js
+++ database/migrations/1587089984920_user_image_fk_schema.js
@@ -18,7 +18,7 @@ class UserImageFkSchema extends Schema {
   down () {
     this.table('users', (table) => {
       // reverse alternations
-      table.dropForeign('image_id')
+      table.dropForeign('image_id') 
     })
   }
 }
