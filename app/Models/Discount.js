0f1b24e1affe721871e8af5c4d6171f4019564af
RafaelFantinel <rafaeldeoliveirafantinel@hotmail.com>
2020-04-18 18:41:14 -0300

    Model de Descontos parcialmente pronto

---
+++ app/Models/Discount.js
@@ -0,0 +1,23 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Discount extends Model {
+    static get table(){
+        return 'coupon_order';
+    }
+    
+    order(){
+        //PK order_id
+        //Nome da PK na tabela de order é id
+        //Primeiro parametro é o Model, o segundo é o campo 
+        //na tabela que contém o valor da PK na tabela deles
+        return this.belongsTo('App/Models/Order','order_id','id')
+    }
+    coupon(){
+        return this.belongsTo('App/Models/Coupon','coupon_id','id')
+    }
+}
+
+module.exports = Discount
