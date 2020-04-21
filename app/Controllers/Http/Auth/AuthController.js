e8cddcc4a819e71b15d4919e7ffb5bdc0b65ec5a
RafaelFantinel <rafaeldeoliveirafantinel@hotmail.com>
2020-04-21 00:48:52 -0300

    Criando Metodo de Deslogar no Controller

--- app/Controllers/Http/Auth/AuthController.js
+++ app/Controllers/Http/Auth/AuthController.js
@@ -36,6 +36,14 @@ class AuthController {
         return response.send({ data: user })
     }
     async logout({ request, response, auth }) {
+        let refresh_token = request.input('refresh_token');
+
+        if (!refresh_token) {
+            refresh_token = request.header('refresh_token')
+        }
+        const loggedOut = await auth.authenticator('jwt').revokeTokens([refresh_token], true); // "true" deleta o token da base
+        
+        return response.status(204).send({ })//204 Indica que foi com sucesso e que a função é de retorno void
 
     }
     async forgot({ request, response }) {
