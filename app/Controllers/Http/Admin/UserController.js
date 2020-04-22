37e9f30d6253d52b29961d4edd29c1c44e0472ef
RafaelFantinel <rafaeldeoliveirafantinel@hotmail.com>
2020-04-22 19:03:41 -0300

    Implementado metodos de Atualizar e Deletar Usuario

--- app/Controllers/Http/Admin/UserController.js
+++ app/Controllers/Http/Admin/UserController.js
@@ -102,7 +102,22 @@ class UserController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update({ params, request, response }) {
+  async update({ params: { id}, request, response }) {
+    const user = await User.findOrFail(id)
+
+
+    const userData  = request.only([
+    'name', 
+    'surname',
+    'email',
+    'password',
+    'image_id'
+  ])
+
+  user.merge(userData)
+
+  await user.save()
+  return response.send(user)
   }
 
   /**
@@ -113,7 +128,15 @@ class UserController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy({ params, request, response }) {
+  async destroy({ params:{ id}, request, response }) {
+    const user = await User.findOrFail(id)
+    try {
+      await user.delete()
+      return response.status(204).send()
+      
+    } catch (error) {
+      return status(500).send({message: 'Não foi possivel deletar ao usuario'})
+    }
   }
 }
 
