69b820c3c8fc829dd8280b0a77caa5c82282f9b1
RafaelFantinel <rafaeldeoliveirafantinel@hotmail.com>
2020-05-04 21:10:09 -0300

    Implementado parcialmente mensagens personalizadas de erros no Validator

--- app/Validators/Auth/Register.js
+++ app/Validators/Auth/Register.js
@@ -1,12 +1,23 @@
 'use strict'
 
 class AuthRegister {
-  get rules () {
+  get rules() {
     return {
-      name:'required',
+      name: 'required',
       surname: 'required',
       email: 'required|email|unique:users,email',//unique:tabela,coluna
       password: 'required|confirmed'//Confirmed = confirmação de senha
+    }
+  }
+  get message() {
+    return {
+      'name.required': 'O nome é obrigatório',
+      'surname.required': 'O sobrenome é obrigatório',
+      'email.required': 'O e-mail é obrigatório',
+      'email.email': 'E-mail inválido',
+      'email.unique': 'Este e-mail já existe',
+      'password.required': 'A senha é obrigatória',
+      'password.confirmed': 'As senhas não são iguais!'
     }
   }
 }
