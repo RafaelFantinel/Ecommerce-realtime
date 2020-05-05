47d05e7e0e209043d3372948da8c98c4b2244744
RafaelFantinel <rafaeldeoliveirafantinel@hotmail.com>
2020-05-05 20:29:08 -0300

    Corrigindo nome de metodo no Hook de OrderItem

--- app/Models/Hooks/OrderItemHook.js
+++ app/Models/Hooks/OrderItemHook.js
@@ -1,9 +1,9 @@
-'use strict'
+  'use strict'
 
 const OrderItemHook = (exports = module.exports = {})
 const Product = use('App/Models/Product')
 
-OrderItemHook.method = async model => {
+OrderItemHook.updateSubtotal = async model => {
   const product = await Product.find(model.product_id)
   model.subtotal = model.quantity * product.price
 }
