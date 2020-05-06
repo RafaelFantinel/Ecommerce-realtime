930dc6fb714f8243c6e83f3b18128e7feeeb912e
RafaelFantinel <rafaeldeoliveirafantinel@hotmail.com>
2020-05-06 19:09:02 -0300

    Correção erro de iimportação de Model de Pedidos na Model de Order

--- app/Models/Order.js
+++ app/Models/Order.js
@@ -4,27 +4,27 @@
 const Model = use('Model')
 
 class Order extends Model {
-  static boot () {
+  static boot() {
     super.boot()
 
     this.addHook('afterFind', 'OrderHook.updateValues')
     this.addHook('afterPaginate', 'OrderHook.updateCollectionValues')
   }
 
-  user () {
-    return this.belongsTo('App/Models/user', 'user_id', 'id')
-  }
-
-  items () {
+  items() {
     return this.hasMany('App/Models/OrderItem')
   }
 
-  coupons () {
+  coupons() {
     return this.belongsToMany('App/Models/Coupon')
   }
 
-  discount () {
+  discounts() {
     return this.hasMany('App/Models/Discount')
+  }
+
+  user() {
+    return this.belongsTo('App/Models/User', 'user_id', 'id')
   }
 }
 
